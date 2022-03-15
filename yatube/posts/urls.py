f24deb2e112135e7c2268a6fb9923d20d2955289
Rafael Aslanyan <rafaaslan923@gmail.com>
2022-03-16 01:08:49 +0300

    Before sprint4 review1

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('profile/<str:username>/', views.profile, name='profile'),
     # Просмотр записи
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    # Создание записи
+    path('posts/create/', views.post_create, name='post_create'),
+    # Редактирование записи
+    path('posts/<post_id>/edit/', views.post_edit, name='post_edit'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
+from .forms import PostForm
 from .models import Post, Group, User
 
 LIMIT_ITEMS = 10
@@ -30,7 +32,7 @@ def group_posts(request, slug):
         'group': group,
         'page_obj': page_obj,
     }
-    return render(request, template , context)
+    return render(request, template, context)
 
 
 def profile(request, username):
@@ -54,3 +56,41 @@ def post_detail(request, post_id):
         'post': post,
     }
     return render(request, 'posts/post_detail.html', context)
+
+
+def post_create(request):
+    # Проверяем, получен POST-запрос или какой-то другой:
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('posts:profile', username=request.user.username)
+        return render(request, 'posts/create_post.html', {'form': form})
+    form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    # Проверяем, получен POST-запрос или какой-то другой:
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('posts:profile', username=request.user.username)
+        return render(request, 'posts/update_post.html', {'form': form,'id':post_id})
+    form = PostForm(instance=post)
+    context = {
+        'form': form,
+        'id': post_id,
+               }
+    return render(request, 'posts/update_post.html', context)
